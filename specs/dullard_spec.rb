9d535b497dd46b5e94097df1091784a71519c92e
Edward Kaplan <ekaplan@mdlogix.com>
2012-03-15 00:08:04 -0400

    README and specs

---
+++ specs/dullard_spec.rb
@@ -0,0 +1,27 @@
+require './dullard'
+
+describe "dullard" do
+  before(:each) do
+    @file = File.open("SHAPE5_CorePrePost.xlsx")
+    @xlsx = Dullard::Workbook.new @file
+  end
+  it "can open a file" do
+    @xlsx.should_not be_nil
+  end
+
+  it "can find sheets" do
+    @xlsx.sheets.count.should == 1
+  end
+
+  it "can read rows" do
+    @xlsx.sheets[0].rows.first.count.should >= 300
+  end
+
+  it "reads the right number of rows" do
+    count = 0
+    @xlsx.sheets[0].rows.each do |row|
+      count += 1
+    end
+    count.should == 115
+  end
+end
